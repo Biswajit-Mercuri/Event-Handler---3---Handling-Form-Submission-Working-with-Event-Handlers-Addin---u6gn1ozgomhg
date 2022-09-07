a3394397230e1b6a3465af48d0f69fae83f2addf
mercuri <mercuri90028@gmail.com>
2022-09-07 08:57:06 +0530

    try to run

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
 
   const handleInput = (event) =>{
-   // use console.log
+    // use console.log
+   event.preventDefault();
    console.log(event.target.value);
   }
 
@@ -12,10 +13,8 @@ const App = () => {
     <div id="main">
       <label htmlFor='text-input'>Text Input:- </label>
       <input id="text-input" type={'text'} onChange={handleInput}/>
-
       <br/>
       <br/>
-
       <label htmlFor='num-input'>Number input</label>
       <input id="num-input"  type={'number'} onChange={handleInput}/>
       <br/>
